bc8acb0f6a28be5f45939e90b98ccd126eb150b3
Keval Malde <maldekeval@ymail.com>
2018-06-27 17:43:31 +0530

    Update card_checker.py

--- card_checker.py
+++ card_checker.py
@@ -1,20 +1,38 @@
 # Vefify check for name of issuer there are many issuer which are not listed below
 def verify(p,q,r,s):
 	if(p==5):
-		if(q==1 or q==2 or q==3 or q==4 or q==5):
+		if(q in [1,2,3,4,5]):
 			print "Mastercard"
-		elif(q==0 or q==6 or q==7 or q==8):
+		elif(q in [0,6,7,8]):
 			print "Maestro"
 	elif(p==4):
 		print "Visa"
 	elif(p==6):
 		if(q==0 or (q==5 and r==2 and s==1)):
 			print "RuPay"
+		elif(q==2):
+			print "China UnionPay"
+		elif(q==4 or q==5):
+			print "Discover Card"
 	elif(p==3):
 		if(q==4 or q==7):
 			print "American Express"
+		elif(q==0 and (r in [0,1,2,3,4,5])):
+			print "Diners Club International"
+		elif(q==0 and r==9 and s==5):
+			print "Diners Club International"
+		elif(q==8 or q==9):
+			print "Diners Club International"
+		elif(q==5 and (r in setj)):
+			print "JCB"
+	elif(p==2):
+		if(q==2 and r==0 and (s in [0,1,2,3,4])):
+			print "MIR"
 # only valid for 16 digit card number
 print("Enter your 16 Digit Card Number:"),
+setj=[]
+for i in range(28,90):
+	setj.append(i)
 ccn=int(raw_input())
 if(len(str(ccn))!=16):
 	print "Unable to verify please check number"
